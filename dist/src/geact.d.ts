2b70af57ff536ee9de009fcb6d6a50fde02ef565
garebareDA <garedaiki@gmail.com>
2021-04-18 14:13:32 +0000

    fix

---
+++ dist/src/geact.d.ts
@@ -0,0 +1,5 @@
+import createElement from './element';
+declare const Geact: {
+    createElement: typeof createElement;
+};
+export default Geact;
---
+++ dist/src/geact.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const element_1 = require("./element");
+const Geact = {
+    createElement: element_1.default
+};
+exports.default = Geact;
